a4ad54e002a2afdb07af8bbbae74684591121275
serap erkan <serapperkan@gmail.com>
2023-07-12 17:48:17 +0300

    .

---
+++ e-commerce/pages/api/admin/products/changeStatus.js
@@ -0,0 +1,12 @@
+
+import Product from "@/models/Product";
+import request from "@/services/request";
+
+
+export default function handle(req, res) {
+    request(res, async () => {
+
+
+
+    })
+}
